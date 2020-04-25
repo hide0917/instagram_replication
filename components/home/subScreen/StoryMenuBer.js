6010538725468b67c506ef94570ad8c1a5384acf
hide0917 <hideforgit@gmail.com>
2020-04-25 18:01:34 +0900

    StoryMenuBar作成

--- components/home/subScreen/StoryMenuBer.js
+++ components/home/subScreen/StoryMenuBer.js
@@ -1,45 +1,105 @@
 import React from "react";
 import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const activeGradientColor = [
+  "hsl(307,95%,35%)",
+  "hsl(351,72%,50%)",
+  "hsl(34,95%,63%)",
+];
+const passiveGradientColor = ["hsl(120,0%,100%)", "hsl(120,0%,100%)"];
+const gradientStartCoordinate = { x: 1, y: 0 };
+const gradientEndCoordinate = { x: 0, y: 1 };
 
 class StoryMenuBar extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView horizontal={true}>
-          <View>
-            <Image
-              style={styles.avator}
-              source={require("../../../assets/accounts/Richard.jpg")}
-            />
+        <ScrollView
+          horizontal={true}
+          directionalLockEnabled={true}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.avator}>
+            <LinearGradient
+              style={styles.linear}
+              colors={passiveGradientColor}
+              start={gradientStartCoordinate}
+              end={gradientEndCoordinate}
+            >
+              <View style={styles.dummyLinear}>
+                <Image
+                  style={styles.avatorImage}
+                  source={require("../../../assets/avator/Richard.jpg")}
+                />
+              </View>
+            </LinearGradient>
             <Text style={styles.avatorName}>ストーリーズ</Text>
           </View>
-          <View>
-            <Image
-              style={styles.avator}
-              source={require("../../../assets/accounts/Richard.jpg")}
-            />
-            <Text style={styles.avatorName}>Richard</Text>
+          <View style={styles.avator}>
+            <LinearGradient
+              style={styles.linear}
+              colors={activeGradientColor}
+              start={gradientStartCoordinate}
+              end={gradientEndCoordinate}
+            >
+              <View style={styles.dummyLinear}>
+                <Image
+                  style={styles.avatorImage}
+                  source={require("../../../assets/avator/Erlich.jpg")}
+                />
+              </View>
+            </LinearGradient>
+            <Text style={styles.avatorName}>erlich</Text>
           </View>
-          <View>
-            <Image
-              style={styles.avator}
-              source={require("../../../assets/accounts/Richard.jpg")}
-            />
-            <Text style={styles.avatorName}>Richard</Text>
+          <View style={styles.avator}>
+            <LinearGradient
+              style={styles.linear}
+              colors={activeGradientColor}
+              start={gradientStartCoordinate}
+              end={gradientEndCoordinate}
+            >
+              <View style={styles.dummyLinear}>
+                <Image
+                  style={styles.avatorImage}
+                  source={require("../../../assets/avator/Gilfoyle.jpg")}
+                />
+              </View>
+            </LinearGradient>
+            <Text style={styles.avatorName}>gilfoyle</Text>
           </View>
-          <View>
-            <Image
-              style={styles.avator}
-              source={require("../../../assets/accounts/Richard.jpg")}
-            />
-            <Text style={styles.avatorName}>Richard</Text>
+          <View style={styles.avator}>
+            <LinearGradient
+              style={styles.linear}
+              colors={activeGradientColor}
+              start={gradientStartCoordinate}
+              end={gradientEndCoordinate}
+            >
+              <View style={styles.dummyLinear}>
+                <Image
+                  style={styles.avatorImage}
+                  source={require("../../../assets/avator/Dinesh.jpg")}
+                />
+              </View>
+            </LinearGradient>
+            <Text style={styles.avatorName}>dinesh</Text>
           </View>
-          <View>
-            <Image
-              style={styles.avator}
-              source={require("../../../assets/accounts/Richard.jpg")}
-            />
-            <Text style={styles.avatorName}>Richard</Text>
+          <View style={styles.avator}>
+            <LinearGradient
+              style={styles.linear}
+              colors={activeGradientColor}
+              start={gradientStartCoordinate}
+              end={gradientEndCoordinate}
+            >
+              <View style={styles.dummyLinear}>
+                <Image
+                  style={styles.avatorImage}
+                  source={require("../../../assets/avator/Jared.jpg")}
+                />
+              </View>
+            </LinearGradient>
+            <Text style={styles.avatorName}>jared</Text>
           </View>
         </ScrollView>
       </View>
@@ -61,15 +121,33 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
   },
   avator: {
-    marginTop: 10,
-    marginLeft: 13,
-    marginRight: 14,
-    marginBottom: 10,
+    width: 82,
+    height: 98,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  linear: {
+    width: 64,
+    height: 64,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 32,
+  },
+  dummyLinear: {
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "hsl(120,0%,100%)",
+    borderRadius: 50,
+  },
+  avatorImage: {
     width: 55,
     height: 55,
     borderRadius: 50,
   },
   avatorName: {
+    marginTop: 4,
     textAlign: "center",
     fontSize: 12,
   },
